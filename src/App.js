2310d11a20fd8628868682a1dcaa4416c4c1724e
Aviral Tandon <aviraltandon20@gmail.com>
2020-07-04 13:25:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
         const data = await api_call.json();
         if (city && country) {
             if(data.cod==="404") {
